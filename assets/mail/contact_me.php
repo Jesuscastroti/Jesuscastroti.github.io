d311ebf1e4ba3440d2f5417736981c9e709e1254
Jesuscastroti <jesusdavidcastroolaya@gmail.com>
2020-07-09 10:31:00 -0500

    agregando icono de wathsapp

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -14,7 +14,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = "jesusdavidcastroolaya@gmail.com"; // Add your email address in between the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
 $subject = "Formulario de contacto web:  $name";
 $body = "Recibiste un nuevo mensaje del formulario de contacto de tu sitio web.\n\n"."Aquí están los detalles:\n\nNombre: $name\n\nEmail: $email\n\nTelefono: $phone\n\nMensaje:\n$message";
-$header = "From: noreply@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$header = "From: jesusdavidcastroolaya@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $header .= "Reply-To: $email";	
 
 if(!mail($to, $subject, $body, $header))
